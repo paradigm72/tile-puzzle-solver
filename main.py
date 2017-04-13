66c0beecb7ccf1cbe8afda36d27b0afae1ebca9c
Paul Romine <promine@gmail.com>
2017-04-12 21:46:58 -0500

    Write up the data array for the tiles. Unsure whether ascii encoding will work for match checking.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # represent a quilt square as a 4-tuple in TRBL orientation:
-# 1=bluebottom1, 2=bluebottom2, 3=whitebottom1, 4=whitebottom2
-# a=bluetop1, b=bluetop2, c=whitetop1, d=whitetop2
+# 1=bluebottomtulips, 2=bluebottomdaisys, 3=whitebottomhearts, 4=whitebottomflowers
+# a=bluetoptulips, b=bluetopdaisys, c=whitetophearts, d=whitetopflowers
 
 # given the set of 9 squares, try each rotation by brute force
+
+import math
 
 class Square:
 	def __init__(self, layout):
@@ -13,25 +15,28 @@ class Square:
 
 
 def doCodesMatch(code1,code2):
-	if code1 == '1' and code2 == 'a':
-		return True
-	elif code1 == '2' and code2 == 'b':
-		return True
-	elif code1 == '3' and code2 == 'c':
-		return True
-	elif code1 == '4' and code2 == 'd':
-		return True
-	elif code1 == 'a' and code2 == '1':
-		return True
-	elif code1 == 'b' and code2 == '2':
-		return True
-	elif code1 == 'c' and code2 == '3':
-		return True
-	elif code1 == 'd' and code2 == '4':
-		return True
-	else:
-		return False
-	return False
+# if code1 == '1' and code2 == 'a':
+# 	return True
+# elif code1 == '2' and code2 == 'b':
+# 	return True
+# elif code1 == '3' and code2 == 'c':
+# 	return True
+# elif code1 == '4' and code2 == 'd':
+# 	return True
+# elif code1 == 'a' and code2 == '1':
+# 	return True
+# elif code1 == 'b' and code2 == '2':
+# 	return True
+# elif code1 == 'c' and code2 == '3':
+# 	return True
+# elif code1 == 'd' and code2 == '4':
+# 	return True
+# else:
+# 	return False
+# return False
+    if (ord(code1) - 96 == code2) or (ord(code2) - 96 == code1):
+        return True
+    return False
 
 def areCompatible(Square1,Square2,Direction):
 	if Direction == 'right':
@@ -49,14 +54,14 @@ def areCompatible(Square1,Square2,Direction):
 
 def initializeSquares():
 	squaresList = [];
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
-	squaresList.append(Square(''))
+	squaresList.append(Square('dcb2'))
+	squaresList.append(Square('2c1d'))
+	squaresList.append(Square('3b4a'))
+	squaresList.append(Square('24ac'))
+	squaresList.append(Square('3124'))
+	squaresList.append(Square('31db'))
+	squaresList.append(Square('243a'))
+	squaresList.append(Square('4ba1'))
+	squaresList.append(Square('4acb'))
 
 
